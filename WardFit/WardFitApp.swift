747818dcc6e48f64fed6f772190586ac0c1c2d27
Tomoya Kureno <tomo7.dev@gmail.com>
2026-04-21 00:12:16 +0700

    Initial Commit

---
+++ WardFit/WardFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WardFitApp.swift
+//  WardFit
+//
+//  Created by Fathariq Dimas on 21/04/26.
+//
+
+import SwiftUI
+
+@main
+struct WardFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
